49d5fc7af617b9768ed247d08d82f132ea545611
zia <ziaurrehman666666@gmail.com>
2026-05-20 21:41:58 +0500

    Fix Next.js workspace root warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
